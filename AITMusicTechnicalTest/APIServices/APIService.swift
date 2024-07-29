7f852ccabd56428d9187f10010d62a1f2367a5f0
apicahaya <106047397+Agni-AIT@users.noreply.github.com>
2024-07-29 16:55:05 +0700

    feat: create api service and error enum

---
+++ AITMusicTechnicalTest/APIServices/APIService.swift
@@ -0,0 +1,79 @@
+//
+//  APIService.swift
+//  AITMusicTechnicalTest
+//
+//  Created by Agni Muhammad on 29/07/24.
+//
+
+import Foundation
+
+enum APIError: Error, Equatable {
+    case invalidURL
+    case networkError(Error)
+    case decodingError(Error)
+    
+    static func == (lhs: APIError, rhs: APIError) -> Bool {
+        switch (lhs, rhs) {
+        case (.invalidURL, invalidURL):
+            return true
+        case (.networkError(let lhsError), .networkError(let rhsError)):
+            return lhsError.localizedDescription == rhsError.localizedDescription
+        case (.decodingError(let lhsError), .decodingError(let rhsError)):
+            return lhsError.localizedDescription == rhsError.localizedDescription
+        default: return false
+        }
+    }
+}
+
+class APIService {
+    static let shared = APIService()
+    var session: URLSession
+    var urlComponents: URLComponents?
+    
+    init(session: URLSession = .shared) {
+        self.session = session
+        self.urlComponents = URLComponents(string: "https://itunes.apple.com/search")
+    }
+    
+    func searchSongs(term: String, completion: @escaping (Result<[Song], APIError>) -> Void) {
+        guard var urlComponents = self.urlComponents else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        urlComponents.queryItems = [
+            URLQueryItem(name: "term", value: term),
+            URLQueryItem(name: "country", value: "ID"),
+            URLQueryItem(name: "media", value: "music")
+        ]
+        
+        guard let url = urlComponents.url else {
+            completion(.failure(.invalidURL))
+            return
+        }
+        
+        let request = URLRequest(url: url)
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            if let error = error {
+                completion(.failure(.networkError(error)))
+            }
+            
+            guard let data = data else {
+                completion(.failure(.networkError(NSError(domain: "No Data", code: 0, userInfo: nil))))
+                return
+            }
+            
+            do {
+                let decoder = JSONDecoder()
+                let searchResponse = try decoder.decode(iTunesSearchResponse.self, from: data)
+                let songs = searchResponse.results.map { Song(id: $0.trackId, title: $0.trackName, artist: $0.artistName, previewURL: $0.previewURL, artworkURL: $0.artworkURL100) }
+                completion(.success(songs))
+                
+            } catch {
+                completion(.failure(.decodingError(error)))
+            }
+        }
+        task.resume()
+    }
+}
